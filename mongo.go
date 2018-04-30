14fd2f05b11355bad77243f3f4979508d2affb7e
peng1996 <33412356+peng1996@users.noreply.github.com>
2018-04-30 21:04:19 +0800

    D:/codes/test/mongo.go

---
+++ mongo.go
@@ -0,0 +1,279 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const N int = 5
+
+type User struct {
+	State int
+	Name  string
+}
+
+type Que struct {
+	Name string
+}
+type Que1 struct {
+	Name string
+}
+type Que2 struct {
+	Name string
+}
+type Que3 struct {
+	Name string
+}
+
+type Queue struct {
+	data *list.List
+}
+
+var queues []Que
+
+//var l = list.New()
+var l1 = list.New()
+var l2 = list.New()
+var l3 = list.New()
+
+func NewQueue() *Queue {
+	q := new(Queue)
+	q.data = list.New()
+	return q
+}
+
+func (q *Queue) push(v interface{}) {
+	defer lock.Unlock()
+	lock.Lock()
+	q.data.PushFront(v)
+}
+
+func (q *Queue) pop() interface{} {
+	defer lock.Unlock()
+	lock.Lock()
+	iter := q.data.Back()
+	v := iter.Value
+	q.data.Remove(iter)
+	return v
+}
+
+func (q *Queue) dump() {
+	for iter := q.data.Back(); iter != nil; iter = iter.Prev() {
+		fmt.Println("item:", iter.Value)
+	}
+}
+
+var lock sync.Mutex
+
+func que() {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	q := session.DB("test").C("user")
+	//qo := session.DB("test").C("queue")
+
+	for true {
+		result := Que{}
+		err = q.Find(bson.M{"state": 0}).One(&result)
+		if err != nil {
+			break
+			log.Fatal(err)
+		}
+		//l.PushBack(result.Name)
+		err = q.Update(
+			bson.M{"name": result.Name},
+			bson.M{"$set": bson.M{"state": 1}})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c := session.DB("test").C("queue")
+		err = c.Insert(&Que{result.Name})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	/*qu := NewQueue()
+	for true {
+
+		res := Que{}
+		err = c.Find(nil).One(&res)
+		if err != nil {
+			break
+			log.Fatal(err)
+		}
+		qu.push(res.Name)
+	}
+	qu.push("hi")
+	qu.dump()*/
+}
+func que1(qu *Queue) {
+	for true {
+
+		if qu.data.Back() == nil {
+			break
+		}
+		x := qu.pop().(string)
+		//var x = qu.pop().(string)
+		//x := err.(string)
+
+		l1.PushBack(x)
+		session, err := mgo.Dial("localhost:27017")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		// Optional. Switch the session to a monotonic behavior.
+		session.SetMode(mgo.Monotonic, true)
+		ss := session.DB("test").C("queue1")
+		err = ss.Insert(&Que1{x})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(x)
+		if l1.Len() == N {
+			var balance [N]string
+			for i := 0; i < N; i++ {
+				balance[i] = l1.Front().Value.(string)
+				//fmt.Printf(l1.Front().Value.(string))
+				l1.Remove(l1.Front())
+			}
+			var str string = "-"
+			for i := 0; i < N; i++ {
+				str += (balance[i] + "-")
+			}
+
+			fmt.Printf("队列1" + str + "\n")
+			ss.RemoveAll(nil)
+			//l1.Init()
+		}
+	}
+}
+func que2(qu *Queue) {
+	for true {
+
+		if qu.data.Back() == nil {
+			break
+		}
+		x := qu.pop().(string)
+		//var x = qu.pop().(string)
+		//x := err.(string)
+
+		l2.PushBack(x)
+		session, err := mgo.Dial("localhost:27017")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		// Optional. Switch the session to a monotonic behavior.
+		session.SetMode(mgo.Monotonic, true)
+		ss := session.DB("test").C("queue2")
+		err = ss.Insert(&Que2{x})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(x)
+		if l2.Len() == N {
+			var balance [N]string
+			for i := 0; i < N; i++ {
+				balance[i] = l2.Front().Value.(string)
+				//fmt.Printf(l1.Front().Value.(string))
+				l2.Remove(l2.Front())
+			}
+			var str string = "-"
+			for i := 0; i < N; i++ {
+				str += (balance[i] + "-")
+			}
+
+			fmt.Printf("队列2" + str + "\n")
+			ss.RemoveAll(nil)
+			//l1.Init()
+		}
+	}
+}
+func que3(qu *Queue) {
+	for true {
+
+		if qu.data.Back() == nil {
+			break
+		}
+		x := qu.pop().(string)
+		//var x = qu.pop().(string)
+		//x := err.(string)
+
+		l3.PushBack(x)
+		session, err := mgo.Dial("localhost:27017")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		// Optional. Switch the session to a monotonic behavior.
+		session.SetMode(mgo.Monotonic, true)
+		ss := session.DB("test").C("queue3")
+		err = ss.Insert(&Que3{x})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(x)
+		if l3.Len() == N {
+			var balance [N]string
+			for i := 0; i < N; i++ {
+				balance[i] = l3.Front().Value.(string)
+				//fmt.Printf(l1.Front().Value.(string))
+				l3.Remove(l3.Front())
+			}
+			var str string = "-"
+			for i := 0; i < N; i++ {
+				str += (balance[i] + "-")
+			}
+
+			fmt.Printf("队列3" + str + "\n")
+			ss.RemoveAll(nil)
+			//l1.Init()
+		}
+	}
+}
+func main() {
+	go que()
+	time.Sleep(1000 * time.Millisecond)
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	s := session.DB("test").C("queue")
+	err = s.Find(nil).All(&queues)
+	if err != nil {
+		log.Fatal(err)
+	}
+	qu := NewQueue()
+	for i := 0; i < len(queues); i++ {
+		qu.push(queues[i].Name)
+	}
+	go que1(qu)
+	go que2(qu)
+	go que3(qu)
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("over")
+
+}
